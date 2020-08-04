bfcca6ebd49081249a726a8a16dfb939c9bf060e
mmytlk <69180079+mmytlk@users.noreply.github.com>
2020-08-04 11:10:59 +0700

    Add files via upload

---
+++ sirapong-6230401987-lab1/compute_circle_area.py
@@ -0,0 +1,6 @@
+import math
+
+radius=float(input("Enter Radius: "))
+area=math.pi*(radius**2)
+
+print("The area of radius %.1f is %.2f" % (radius,area))
---
+++ sirapong-6230401987-lab1/find_avg_std.py
@@ -0,0 +1,9 @@
+import random, statistics
+
+number1 = random.randint(1, 11)
+number2 = random.randint(1, 11)
+average = number1+number2 / 2
+sd = (statistics.pstdev([number1, number2]))
+
+print("The average of %d and %d is %.1f" %(number1, number2, average))
+print("The standard deviation of %d and %d is %.2f" %(number1, number2, sd))
