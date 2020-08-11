6aecfdb3741408b2f06c9e827a9a2c8eb247a232
mmytlk <69180079+mmytlk@users.noreply.github.com>
2020-08-11 21:39:38 +0700

    Update convert_temperature.py
    delete trailing whitespace at line7

--- sirapong-6230401987-lab2/convert_temperature.py
+++ sirapong-6230401987-lab2/convert_temperature.py
@@ -4,7 +4,7 @@ while not check_input:
     try:
         temp_f = float(input("Enter a temperature in Farenheit: "))
         temp_c = (5 / 9) * (temp_f - 32)
-        print("Temperature %.2f in Farenheit is %.2f in Celsius" 
+        print("Temperature %.2f in Farenheit is %.2f in Celsius"
               % (temp_f, temp_c))
     except ValueError:
         print("Please enter a valid floating point for the temperature.")
