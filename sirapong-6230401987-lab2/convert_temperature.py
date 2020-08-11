1c7cfd5d7468229c0d2c0cc7cc8a95eb3f92dcad
mmytlk <69180079+mmytlk@users.noreply.github.com>
2020-08-11 11:27:50 +0700

    Add files via upload

---
+++ sirapong-6230401987-lab2/convert_temperature.py
@@ -0,0 +1,12 @@
+check_input = False
+
+while not check_input:
+    try:
+        temp_f = float(input("Enter a temperature in Farenheit: "))
+        temp_c = (5 / 9) * (temp_f - 32)
+        print("Temperature %.2f in Farenheit is %.2f in Celsius" 
+              % (temp_f, temp_c))
+    except ValueError:
+        print("Please enter a valid floating point for the temperature.")
+    else:
+        check_input = True
