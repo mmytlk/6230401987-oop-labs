9ba49f6f38d1ab3e99bf485c3ccc318ffb2ed15f
mmytlk <69180079+mmytlk@users.noreply.github.com>
2020-08-18 12:41:10 +0700

    edit code #1

--- sirapong-6230401987-lab3/Problem 4.py
+++ sirapong-6230401987-lab3/Problem 4.py
@@ -1,11 +1,15 @@
-n = int(input("Enter a number to find the factorial :"))
+n = int(input("Enter a number to find the factorial : "))
 
 user_factorial = n
 factorial = 1
+fac01 = 1
 
 while True:
     factorial = n * factorial
     n -= 1
-    if n == 1:
+    if user_factorial == 1 or user_factorial == 0 :
+        print(f"Factorial of {user_factorial} is {fac01}")
+        break
+    elif n == 1:
         print(f"Factorial of {user_factorial} is {factorial}")
         break
