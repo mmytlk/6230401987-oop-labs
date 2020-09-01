16ea403e75b5cf5be9d3cdfd844fa10f361b8ba0
mmytlk <69180079+mmytlk@users.noreply.github.com>
2020-09-01 11:28:14 +0700

    edit code #1

--- sirapong-6230401987-lab5/p6.py
+++ sirapong-6230401987-lab5/p6.py
@@ -9,6 +9,18 @@ def make_greeting(title, name, surname, formal=True, time=None):
         greeting = "Good %s" % time
     return "%s, %s!" % (greeting, fullname)
 
+def make_greeting2(title, name, surname, formal=True, time=None):
+    if formal:
+        fullname = "%s %s" % (title, surname)
+    else:
+        fullname = name
+    if time is None:
+        greeting = "Hello"
+    else:
+        greeting = "Good %s" % time
+    return "%s, %s!" % (greeting, fullname)
+
+
 my_dict = {
     "title": "Mr",
     "name": "John",
@@ -21,4 +33,4 @@ print(make_greeting("Mr", "John", "Smith"))
 print(make_greeting("Mr", "John", "Smith", False))
 print(make_greeting("Mr", "John", "Smith", False, "evening"))
 print(make_greeting("Mr", "Mana", "Deejai", True, None))
-print(make_greeting(**my_dict))
+print(make_greeting2(**my_dict))
